3057800062b34914f18fb77f3a9688245c92c155
marius-arch <mari.starke@gmail.com>
2022-11-08 08:32:06 +0100

    fixed lcd display and added debugging options

--- dht11.py
+++ dht11.py
@@ -5,8 +5,11 @@ import board
 import time
  
 from adafruit_ht16k33.segments import Seg7x4
-from Adafruit_CharLCD import Adafruit_CharLCD
+import adafruit_character_lcd.character_lcd_i2c as character_lcd
 from statistics import median
+
+# show that script has started
+print("Script has started. Version: v.1.0.0")
  
 ## initialize GPIO
 # disable warnings for ports that are already in use
@@ -15,10 +18,10 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 # cleanup all the used ports
 GPIO.cleanup()
-
+ 
 # configure on which gpio pin the dht11 sensor is located
 instance = dht11.DHT11(pin = 4)
-
+ 
 ## initialize 7-segmengt display
 # create the i2c interface
 i2c = board.I2C()
@@ -26,12 +29,15 @@ i2c = board.I2C()
 segment = Seg7x4(i2c, address=0x70)
 # clear the 7 segment display
 segment.fill(0)
-
+ 
 ## initialize lcd display
-lcd = Adafruit_CharLCD(rs=26, en=19, d4=13, d5=6, d6=5, d7=11, cols=16, lines=2)
+lcd = character_lcd.Character_LCD_I2C(i2c, 16, 2, 0x21)
 # clear the lcd display
 lcd.clear()
 
+# pass-trough number for console debugging
+passTrough = 1
+ 
 # while 1 continiously runs the code inside of it, to make sure the measured values are up-to-date
 while 1:
     # create array for temp and humidity values
@@ -46,25 +52,32 @@ while 1:
         # add the values to the arrays
         temperatures.append(result.temperature)
         humidities.append(result.humidity)
-
+ 
     # create the median of the values to avoid deviations and make the values global
     global temperature
     temperature = median(sorted(temperatures))
     global humidity
     humidity = median(sorted(humidities))
-
+    
+    print("Current run: {}".format(passTrough))
+ 
     # prints the current measured temperature and humidity with one decimal place for testing purposes
-    print("Temperature: {:.1f} °C".format(temperature))
-    print("Humidity: {:.1f} %".format(humidity))
+    print("Temperature: {:.1f}°C".format(temperature))
+    print("Humidity: {:.0f}% \n".format(int(humidity)))
  
     # configure what each segment of the display should show
     segment[0] = str(int(temperature / 10))
     segment[1] = str(int(temperature % 10))
     segment[2] = str(int(humidity / 10))
     segment[3] = str(int(humidity % 10))
-
+ 
+    # turn on backlight of lcd display
+    lcd.backlight = True
     # display the temperature and humidity on the lcd display with one decimal place
-    lcd.message('Temp={:.1f}°C  \nHumidity={:.1f}%'.format(temperature, humidity))
+    lcd.message = "Temp= {:.1f}".format(temperature) + chr(223) + "C\n" + "Humidity= {:.0f}%".format(humidity)
     
+    # increase pass-trough number
+    passTrough+=1
+ 
     # wait 200ms until continuing
-    time.sleep(0.2)
+    #time.sleep(0.2)
